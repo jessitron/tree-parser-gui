79027b439848773bb395e236b04fafd7122487ad
wallheac <wallheac@gmail.com>
2018-12-30 00:04:28 -0600

    refactored code, added code display, and imported prism.js(not working yet)

---
+++ app/components/codeSubmission/codeDisplay.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export class CodeDisplay extends React.Component<{
+    dataToParse: {
+        code: string, 
+        parserChoice: string}
+    }, {}> {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+        return(
+            <div>
+                <pre>
+                    <code className={`lang-java`}>
+                        {this.props.dataToParse && this.props.dataToParse.code || ""}
+                    </code>
+                </pre>
+            </div>
+        );
+    }
+}
