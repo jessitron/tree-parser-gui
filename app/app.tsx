696899262e52e73f544c1e3a6ac29f6873048b9e
wallheac <wallheac@gmail.com>
2019-01-06 13:22:44 -0600

    Atomistified the styling

--- app/app.tsx
+++ app/app.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { TreeParseGUI } from "./components/TreeParseGUI"
+import { TreeParseGUI } from "./components/TreeParseGUI";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import theme from '../static/theme'
 
-ReactDOM.render(<TreeParseGUI />, document.getElementById('main'));
+ReactDOM.render(<MuiThemeProvider theme={theme}><TreeParseGUI /></MuiThemeProvider>, document.getElementById('main'));
